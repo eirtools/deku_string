fae5daec04a156b51da8b89946a0a8761110696d
Eir Nym <485399+eirnym@users.noreply.github.com>
2025-04-20 20:38:53 +0200

    feat: Stable vector format VecDeku implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,72 +2,43 @@
 #![deny(missing_debug_implementations)]
 
 //!
-//! Simple wrapper around String to implement `DekuReader` and `DekuWriter`
+//! Simple wrapper around String and Vec<T> implementing `DekuReader` and `DekuWriter`.
 //!
-//! This implementation requires following context:
-//!  * Endian — little or big
-//!  * Encoding — UTF-8 or UTF-16
-//!  * `StringLayout` — how string is represented in binary
-//!
-//! Pick your favorite combination
-//!
-//! Example usage od `StringDeku` with UTF-8 strings:
+//! This implementation guarantees:
+//!  * Predictable reads and writes
+//!  * For fixed length layouts it will be always read and written with that size (if data is valid)
+//!  * No forgotten update()
+//!  * Suitable for nested structures like this: `Encrypted<Compressed<FixedLengthVec<PascalString>>>`
 //!
 //! ```rust,ignore
-//! use ::deku_string::{StringDeku, Encoding, StringLayout, Size};
+//! use ::deku_string::{Encoding, Size, StringDeku, StringLayout, VecLayout};
 //!
 //! #[derive(Debug, Clone, PartialEq, ::deku::DekuRead, ::deku::DekuWrite)]
 //! #[deku(endian = "little")]
 //! struct SampleModel {
-//!     // fixed length buffer, null  character is required to be inside
-//!     // "012345678\x00" is allowed
-//!     // "0123456789" is NOT allowed
 //!     #[deku(ctx = "Encoding::Utf8, StringLayout::fixed_length(10)")]
-//!     fixed_value1: StringDeku,
+//!     fixed_string: StringDeku,
 //!
-//!     // fixed length buffer, null byte is allowed to be inside,
-//!     // both "012345678\x00" and "0123456789" are allowed
-//!     #[deku(ctx = "Encoding::Utf8, StringLayout::FixedLength{size: 10, allow_no_null: true}")]
-//!     fixed_value2: StringDeku,
-//!
-//!     // length (1 byte) then string, null character is NOT allowed inside
-//!     // b"\0x501234"
-//!     #[deku(ctx = "Encoding::Utf8, StringLayout::LengthPrefix(Size::U8)")]
-//!     prefixed_u8: StringDeku,
-//!
-//!     // length (2 byte) then string, null character is NOT allowed inside
-//!     // b"\0x5\x0001234"
-//!     #[deku(ctx = "Encoding::Utf8, StringLayout::LengthPrefix(Size::U16)")]
-//!     prefixed_u16: StringDeku,
-//!
-//!     // length (4 byte) then string, null character is NOT allowed inside
-//!     // b"\0x5\x00\x00\x0001234"
-//!     #[deku(ctx = "Encoding::Utf8, StringLayout::LengthPrefix(Size::U32)")]
-//!     prefixed_u32: StringDeku,
-//!
-//!     // String with null byte at the end
-//!     // b"012345\x00"
-//!     #[deku(ctx = "Encoding::Utf8, StringLayout::ZeroEnded")]
-//!     zero_ended: StringDeku,
+//!     #[deku(ctx = "VecLayout::LengthPrefix(Size::U32), (Encoding::Utf8, StringLayout::ZeroEnded)")]
+//!     prefixed_u32_vec: VecDeku<StringDeku>,
 //! }
 //! ```
 
 mod deku_impl;
+mod deku_impl_vec;
 mod std_impl;
 
 ///
-/// Simple wrapper around String to read and write various layouts.
+/// Thin wrapper around String to read and write various layouts.
 /// This looks as a simple wrapper over String, `StringDeku` structure is built
 /// to be able of reading and writing of various common binary layouts.
-///
-/// For example,
 ///
 /// * Fixed Length Layout represents a fixed amount of elements (bytes or words depending on encoding)
 ///   to read and write
 /// * Length prefixed layout represent Pascal-like strings
 /// * Zero ended — C-like strings.
 ///
-/// How it's different from using Deku directly?
+/// How it's different from using `deku` directly?
 ///
 /// * It's convenient way to create and maintain models:
 ///    * no readers and writers,
@@ -80,7 +51,28 @@ mod std_impl;
 #[derive(Clone, Default, PartialEq, PartialOrd, Eq, Ord)]
 pub struct StringDeku(pub(crate) String);
 
-/// String variant to read and write
+/// Thin wrapper around Vec<T> to read and write various layouts.
+/// This looks as a simple wrapper over Vec<T>, `VecDeku` structure is built
+/// to be able of reading and writing of various common binary layouts.
+///
+/// * Fixed Length Layout represents a fixed amount of elements (bytes or words depending on encoding)
+///   to read and write.
+/// * Length prefixed layout represent count and then
+///
+/// How it's different from using `deku` directly?
+///
+/// * It's convenient way to create and maintain models:
+/// * no readers and writers,
+/// * no running `update` function, which may be forgotten.
+/// * Fixed length is actually fixed, so if developer asked size 10, it will be read and written exactly 10.
+/// * Easy to write complicated fields like this: `Encrypted<Compressed<FixedLengthVec<PascalString>>>`
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd)]
+pub struct VecDeku<T>(pub Vec<T>)
+where
+    T: Sized + Clone + PartialEq + PartialOrd;
+
+///
+///  Supported string binary layouts
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
 pub enum StringLayout {
@@ -109,6 +101,24 @@ impl StringLayout {
             allow_no_null: false,
         }
     }
+}
+
+///
+///  Supported string binary layouts
+#[derive(Debug, Clone, Copy)]
+#[non_exhaustive]
+pub enum VecLayout {
+    /// Fixed length string
+    FixedLength(
+        /// How many (exact) items should be in vector.
+        usize,
+    ),
+
+    /// String is prefixed by length value
+    LengthPrefix(Size),
+
+    /// Read until the reader end
+    End,
 }
 
 /// Encoding to use for read and write
