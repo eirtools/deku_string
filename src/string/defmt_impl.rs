6732eff980049f6de8e775a29d15b7b72533e8e3
Elliot Sayes <elliot@sayes.cloud>
2025-08-22 23:45:53 +0200

    Move as unit test

--- src/string/defmt_impl.rs
+++ src/string/defmt_impl.rs
@@ -6,3 +6,20 @@ impl Format for StringDeku {
         defmt::write!(fmt, "{}", self.0.as_str());
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::format;
+
+    use super::*;
+
+    const TEST_CONTENT: &str = "test_content";
+
+    #[test]
+    fn format_string_deku() {
+        let string_deku = StringDeku::from(TEST_CONTENT);
+        let formatter = defmt::Display2Format(&string_deku);
+        let output = format!("{}", formatter);
+        assert_eq!(output, TEST_CONTENT);
+    }
+}
